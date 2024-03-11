1044f143c0b3b0a8888521cec908f36982c8d1dc
Pratik Vyavahare <89389983+Pratikv2002@users.noreply.github.com>
2024-03-11 22:34:22 +0530

    add the block sport

--- src/Pages/BlockMarket/BlockSport.js
+++ src/Pages/BlockMarket/BlockSport.js
@@ -1,40 +1,92 @@
-import React,{useState} from 'react'
-import "./BlockSport.css"
+import React, { useState, useEffect } from "react";
+import "jspdf-autotable";
+import Table from "../../Components/Table/Table";
 import { useNavigate, useParams } from "react-router-dom";
+import { FaUserEdit } from "react-icons/fa";
+import { TbPasswordUser } from "react-icons/tb";
+import axios from "../../authAxios";
 
-export default function BlockSport() {
-    const params = useParams();
-    const navigate = useNavigate();
-    const [matches, setmatches] = useState(["a", "b"]);
+const BlockSport = () => {
+  const navigate = useNavigate();
+  const params = useParams();
+  const [filterText, setFilterText] = useState("");
+  const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
+  const [colData, setColData] = useState([]);
+  const [game, setdata] = useState([]);
+  const [btnClickFlag, setBtnClickFlag] = useState(false); 
+  const handleBtnClick = async (_id, status) => {
+    try {
+      var response = await axios.patch("game/updateStatus", { _id, status });
+      alert(response.data.msg);
+      setBtnClickFlag(!btnClickFlag); 
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get("/game/getGames", {});
+        setdata(result.data.dataobj);
+        setColData(result.data.data.dataobj);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, [btnClickFlag]); // Include btnClickFlag in the dependency array
+
+  const subHeaderComponentMemo = React.useMemo(() => {
+    const handleClear = () => {
+      if (filterText) {
+        setResetPaginationToggle(!resetPaginationToggle);
+        setFilterText("");
+      }
+    };
+
+    return (
+      <input
+        style={{ outline: "none" }}
+        placeholder="Search Here"
+        onChange={(e) => setFilterText(e.target.value)}
+        filterText={filterText}
+      />
+    );
+  }, [filterText, resetPaginationToggle]);
+
+  const columns = [
+    { name: "So", selector: (row) => row._id },
+    { name: "BetfairId", selector: (row) => row.gId },
+    { name: "Name", selector: (row) => row.gName },
+    { name: "Status", selector: (row) => (row.block ? row.gName + " is block" : row.gName + " is Unblock") },
+    {
+      name: "Actions",
+      cell: (row) => (
+        <div style={{ display: 'flex', justifyContent: "space-between" }}>
+          <button onClick={() => handleBtnClick(row._id, !row.block)}>{row.block ? "Unblock" : "Block"}</button>
+        </div>
+      ),
+    },
+  ];
+
   return (
-    <div style={{backgroundColor:"#2B2D3A", borderRadius:"10px", padding:"8px 16px"}}>
-        <h5 style={{color:"white"}}>List</h5>
-         <div style={{ overflow: "scroll" }}>
-        <table class="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">So.</th>
-              <th scope="col">BetfairId</th>
-              <th scope="col">Name</th>
-              <th scope="col">Status</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {matches.map((item) => {
-              return (
-                <tr>
-                  <th scope="row">05 Dec 23</th>
-                  <td onClick={() => navigate("/ledger/" + item)}>Mark</td>
-                  <td>00</td>
-                  <td>00</td>
-                  <td>00</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+    <div>
+      <div>
+        <Table
+          data={game}
+          columns={columns}
+          title="Games"
+          pagination
+          subHeader
+          subHeaderComponent={subHeaderComponentMemo}
+          persistTableHead
+          paginationResetDefaultPage={resetPaginationToggle}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default BlockSport;
